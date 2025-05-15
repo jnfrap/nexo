7bd67a1df4a34788a97f75929a5463a8079b858b
jnfrap <jnfr97@gmail.com>
2025-05-15 16:41:33 +0200

    Added custom prime preset

--- src/main.js
+++ src/main.js
@@ -5,14 +5,33 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 const app = createApp(App)
 
 app.use(router)
 
+const customPrimePreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{fuchsia.50}',
+      100: '{fuchsia.100}',
+      200: '{fuchsia.200}',
+      300: '{fuchsia.300}',
+      400: '{fuchsia.400}',
+      500: '{fuchsia.500}',
+      600: '{fuchsia.600}',
+      700: '{fuchsia.700}',
+      800: '{fuchsia.800}',
+      900: '{fuchsia.900}',
+      950: '{fuchsia.950}'
+    }
+  }
+})
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
+    preset: customPrimePreset
   }
 });
 
