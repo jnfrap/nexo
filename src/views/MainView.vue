ce71194799b4bde97084353c92258b3faf009bf5
jnfrap <jnfr97@gmail.com>
2025-05-15 16:27:18 +0200

    Installed PrimeVue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,3 +1,27 @@
+<script>
+import { Button, InputText } from 'primevue';
+
+export default {
+  data() {
+    return {
+      value: "testing"
+    }
+  },
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Button,
+    InputText
+  },
+  methods: {
+    resetName() {
+      this.value = "testing";
+    }
+  }
+}
+</script>
+
 <template>
   <h1 class="text-3xl font-bold text-center text-blue-600 my-8">Main view</h1>
+  <InputText type="text" v-model="value" />
+  <Button label="Submit" @click="resetName()" icon="pi pi-search">Reset</Button>
 </template>
