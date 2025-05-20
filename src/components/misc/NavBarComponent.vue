dfe08ee55354c6a257d6c1c9df695f728a6d6660
jnfrap <jnfr97@gmail.com>
2025-05-20 21:13:42 +0200

    Added fonts

--- src/components/misc/NavBarComponent.vue
+++ src/components/misc/NavBarComponent.vue
@@ -9,7 +9,7 @@
       <a href="/index.html">
         <div class="h-full flex justify-center items-end w-fit">
           <img src="/branding/Nx.png" alt="Logo de Nexo" class="h-full" />
-          <p class="mb-[5px] text-white text-[35px] font-['Raleway']">Nexo</p>
+          <p class="mb-[5px] text-white text-[35px] font-(family-name: Raleway)">Nexo</p>
         </div>
       </a>
       <div class="flex items-center gap-[10px] w-fit">
