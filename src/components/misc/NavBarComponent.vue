71ff5513f439d99eced8063e203d49d81da2ac01
jnfrap <jnfr97@gmail.com>
2025-05-20 21:44:09 +0200

    Added div to justify to the left and a placeholder to separate navbar content from the logo

--- src/components/misc/NavBarComponent.vue
+++ src/components/misc/NavBarComponent.vue
@@ -2,14 +2,17 @@
   <div class="h-[70px]"></div>
   <div class="w-full h-[70px] bg-[#4b164c] p-[5px] flex justify-center box-border fixed top-0 z-[1000]">
     <div class="h-full w-[70%] flex justify-between select-none">
-      <RouterLink to="/">
-        <div class="h-full flex justify-center items-end w-fit">
-          <img src="/branding/Nx.png" alt="Logo de Nexo" class="h-full" />
-          <p class="mb-[5px] text-white text-[35px] font-(family-name: Raleway)">Nexo</p>
-        </div>
-      </RouterLink>
-      <!-- Create bord button -->
-      <!-- Recent boards button -->
+      <div class="flex flex-row justify-start">
+        <RouterLink to="/">
+          <div class="h-full flex justify-center items-end w-fit">
+            <img src="/branding/Nx.png" alt="Logo de Nexo" class="h-full" />
+            <p class="mb-[5px] text-white text-[35px] font-(family-name: Raleway)">Nexo</p>
+          </div>
+        </RouterLink>
+        <div class="w-6"></div>
+        <!-- Create bord button -->
+        <!-- Recent boards button -->
+      </div>
 
       <div class="flex items-center gap-[10px] w-fit">
         <!-- Search engine -->
