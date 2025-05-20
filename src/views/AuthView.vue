08cef71e11c1189cf74f400408e6f0886d19ef2a
javidev <jajavieralonso9@gmail.com>
2025-05-20 09:45:16 +0200

    Import register component to authview

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,15 +1,18 @@
 <script>
+import RegisterComponent from '../components/auth/RegisterComponent.vue';
 import LoginComponent from '../components/auth/LoginComponent.vue';
 
 
 export default {
   name: 'AuthView',
   components: {
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   }
 }
 </script>
 
 <template>
   <LoginComponent />
+  <RegisterComponent />
 </template>
