7bb5fa085e464da496b418591536172de8b7a26c
jnfrap <jnfr97@gmail.com>
2025-05-20 21:25:04 +0200

    Removed unnecessary buttons

--- src/components/misc/NavBarComponent.vue
+++ src/components/misc/NavBarComponent.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="h-[70px]"></div>
-  <div
-    class="w-full h-[70px] bg-[#4b164c] p-[5px] flex justify-center box-border fixed top-0 z-[1000]"
-  >
-    <div
-      class="h-full w-[70%] flex justify-between select-none"
-    >
+  <div class="w-full h-[70px] bg-[#4b164c] p-[5px] flex justify-center box-border fixed top-0 z-[1000]">
+    <div class="h-full w-[70%] flex justify-between select-none">
       <RouterLink to="/">
         <div class="h-full flex justify-center items-end w-fit">
           <img src="/branding/Nx.png" alt="Logo de Nexo" class="h-full" />
           <p class="mb-[5px] text-white text-[35px] font-(family-name: Raleway)">Nexo</p>
         </div>
       </RouterLink>
+      <!-- Create bord button -->
+      <!-- Recent boards button -->
+
       <div class="flex items-center gap-[10px] w-fit">
-        <div
-          class="h-1/2 w-[150px] bg-[#dc7fcc] rounded-full flex justify-center items-center cursor-pointer text-[17px] hover:bg-[#ffcbfa]"
-        >
-          <a href="#prices" class="text-black font-bold">Precio</a>
-        </div>
-        <div
-          class="h-1/2 w-[150px] bg-[#dc7fcc] rounded-full flex justify-center items-center cursor-pointer text-[17px] hover:bg-[#ffcbfa]"
-        >
-          <a href="/index.html" class="text-black font-bold">Iniciar sesión</a>
-        </div>
+        <!-- Search engine -->
+        <!-- Notification button -->
+        <!-- User component -->
       </div>
     </div>
   </div>
