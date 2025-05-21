9b69c9966d104b8d76da306175f933359503f67b
jnfrap <jnfr97@gmail.com>
2025-05-21 20:41:37 +0200

    Added create board button

--- src/components/misc/navbar/CreateBoardButtonComponent.vue
+++ src/components/misc/navbar/CreateBoardButtonComponent.vue
@@ -1,3 +1,27 @@
+<!-- eslint-disable vue/no-reserved-component-names -->
+<script>
+import Button from 'primevue/button';
+
+export default {
+  name: 'CreateBoardButtonComponent',
+  components: {
+    Button
+  },
+  methods: {
+    createBoard() {
+      // Logic to create a new board
+      console.log('Creating a new board...');
+    }
+  }
+}
+</script>
+
 <template>
-  <h1>Test</h1>
+  <Button
+    label="Create Board"
+    icon="pi pi-plus"
+    class="p-button-raised p-button-primary"
+    @click="createBoard"
+  />
 </template>
+
