8b2e584b3658552da7ac00ce7c90d5cdb409a5be
jnfrap <jnfr97@gmail.com>
2025-05-21 13:51:36 +0200

    Added context menu

--- src/components/main/BoardPreviewComponent.vue
+++ src/components/main/BoardPreviewComponent.vue
@@ -2,6 +2,8 @@
 <script>
 import { Button } from 'primevue';
 import Menu from 'primevue/menu';
+import ContextMenu from 'primevue/contextmenu';
+
 
 export default {
   name: 'BoardPreviewComponent',
@@ -31,7 +33,8 @@ export default {
   },
   components: {
     Menu,
-    Button
+    Button,
+    ContextMenu
   },
   props: {
     id: {
@@ -61,7 +64,6 @@ export default {
       this.localIsFavorite = !this.localIsFavorite;
     },
     goToBoard() {
-      console.log('Navigating to board with ID:', this.id);
       // Here use Vue Router to navigate to the board page
     },
     toggleMenu(event) {
@@ -73,25 +75,26 @@ export default {
 
 <template>
   <div class="w-64 h-40 rounded-lg relative overflow-hidden cursor-pointer" @click="goToBoard()"
-    @mouseenter="isHovered = true" @mouseleave="isHovered = false"
+    @mouseenter="isHovered = true" @mouseleave="isHovered = false" @contextmenu="toggleMenu($event)"
     :style="{ backgroundImage: `url(${localBackgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
 
     <div class="absolute inset-0 backdrop-blur-sm transition-opacity duration-300"
       :class="isHovered ? 'opacity-0' : 'opacity-100'"></div>
 
     <div class="absolute top-2 right-2 flex space-x-2">
-      <button class="text-yellow-400 focus:outline-none cursor-pointer"
-      @click.stop="toggleFavorite()">
-      <i v-if="localIsFavorite" class="pi pi-star-fill" title="Unfavorite" style="font-size: 1.5rem"></i>
-      <i v-else class="pi pi-star" title="Favorite" style="font-size: 1.5rem"></i>
+      <button class="text-yellow-400 focus:outline-none cursor-pointer" @click.stop="toggleFavorite()">
+        <i v-if="localIsFavorite" class="pi pi-star-fill" title="Unfavorite" style="font-size: 1.5rem"></i>
+        <i v-else class="pi pi-star" title="Favorite" style="font-size: 1.5rem"></i>
       </button>
 
-      <Button type="button" @click.stop="toggleMenu($event)" aria-haspopup="true"
-      aria-controls="overlay_menu" unstyled class="cursor-pointer text-white">
+      <Button type="button" @click.stop="toggleMenu($event)" aria-haspopup="true" aria-controls="overlay_menu" unstyled
+        class="cursor-pointer text-white">
         <i class="pi pi-ellipsis-v" style="font-size: 1rem"></i>
       </Button>
       <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
     </div>
+
+    <ContextMenu ref="menu" :model="items" />
 
     <div class="absolute bottom-0 left-0 right-0 p-4 text-white bg-gradient-to-t from-black to-transparent">
       <h2 class="font-bold transition-all duration-300" :class="isHovered ? 'text-2xl' : 'text-lg'">
