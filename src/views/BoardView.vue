4367d93fe22fde185625c5ff55995bde787880dd
jnfrap <jnfr97@gmail.com>
2025-05-23 01:09:38 +0200

    Load current board on board view

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -6,12 +6,32 @@ export default {
   components: {
     TaskContainerComponent
   },
+  data() {
+    return {
+      board: {}
+    }
+  },
   methods: {
+  },
+  mounted() {
+    try {
+      const boards = localStorage.getItem('boards') ? JSON.parse(localStorage.getItem('boards')) : [];
+      const boardId = parseInt(this.$route.params.boardId);
+      const board = boards.find(b => b.id === boardId);
+      if (board) {
+        this.board = board;
+      } else {
+        throw new Error(`Board ${boardId} not found`);
+      }
+    } catch (error) {
+      console.error(error);
+      this.$router.push('/');
+    }
   },
 }
 </script>
 
 <template>
-  <h1>Board view</h1>
+  <h1>{{ board.title }}</h1>
   <TaskContainerComponent />
 </template>
