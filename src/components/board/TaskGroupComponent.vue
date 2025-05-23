f3f1a87b531dcb03d14c33be45401a908583d3f4
jnfrap <jnfr97@gmail.com>
2025-05-23 12:22:00 +0200

    Added menu and context menu to TaskGroupComponent

--- src/components/board/TaskGroupComponent.vue
+++ src/components/board/TaskGroupComponent.vue
@@ -1,17 +1,62 @@
+<!-- eslint-disable vue/no-reserved-component-names -->
 <script>
+import Menu from 'primevue/menu';
+import ContextMenu from 'primevue/contextmenu';
+import { Button } from 'primevue';
+
 export default {
   name: 'TaskGroupComponent',
+  components: {
+    Menu,
+    ContextMenu,
+    Button
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          label: 'Edit',
+          icon: 'pi pi-fw pi-pencil',
+          command: () => {
+            // Add logic to edit the board
+          }
+        },
+        {
+          label: 'Delete',
+          icon: 'pi pi-fw pi-trash',
+          command: () => {
+            // Add logic to delete the board
+          }
+        }
+      ]
+    }
+  },
   props: {
     taskGroup: {
       type: Object,
       required: true,
     },
   },
+  methods: {
+    toggleMenu(event) {
+      this.$refs.menu.toggle(event);
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="w-80 rounded-xl p-4 bg-[#f1f2f4] shadow-lg">
-    <h2 class="text-lg">{{ taskGroup.title }}</h2>
+  <div class="w-80 rounded-xl p-4 bg-[#f1f2f4] shadow-lg" @contextmenu="toggleMenu($event)">
+    <div class="flex flex-row justify-between items-center">
+      <h2 class="text-lg">{{ taskGroup.title }}</h2>
+
+      <Button type="button" @click.stop="toggleMenu($event)" aria-haspopup="true" aria-controls="overlay_menu" unstyled
+        class="cursor-pointer">
+        <i class="pi pi-ellipsis-h" style="font-size: 1rem"></i>
+      </Button>
+      <Menu ref="menu" id="overlay_menu" :model="menuItems" :popup="true" />
+    </div>
   </div>
+
+  <ContextMenu ref="menu" :model="menuItems" />
 </template>
