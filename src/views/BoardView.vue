f08221d21c25de8e1a5e8035f2cbfd6b49abbe8a
jnfrap <jnfr97@gmail.com>
2025-05-23 03:01:50 +0200

    Added button to add task

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,25 +1,26 @@
+<!-- eslint-disable vue/no-reserved-component-names -->
 <script>
 import TaskContainerComponent from '@/components/board/TaskContainerComponent.vue';
 import { VueDraggableNext } from 'vue-draggable-next'
+import Button from 'primevue/button';
 
 export default {
   name: 'BoardView',
   components: {
     TaskContainerComponent,
     draggable: VueDraggableNext,
+    Button
   },
   data() {
     return {
       board: {},
-      list: [
-        { name: 'Task 1' },
-        { name: 'Task 2' },
-        { name: 'Task 3' },
-        { name: 'Task 4' },
-      ],
+      tasks: [],
     }
   },
   methods: {
+    addTask() {
+
+    }
   },
   mounted() {
     try {
@@ -28,6 +29,7 @@ export default {
       const board = boards.find(b => b.id === boardId);
       if (board) {
         this.board = board;
+        this.tasks = board.tasks;
       } else {
         throw new Error(`Board ${boardId} not found`);
       }
@@ -42,9 +44,10 @@ export default {
 <template>
   <h1>{{ board.title }}</h1> <!-- This must be in the future second navbar -->
 
-  <draggable :list="list" class="flex flex-row space-x-4 mx-4">
-    <div v-for="e in list" :key="e.name">
-      <TaskContainerComponent :task="{ title: e.name }"/>
+  <draggable :list="tasks" class="flex flex-row space-x-4 mx-4">
+    <div v-for="t in list" :key="t.title">
+      <TaskContainerComponent :task="t" />
     </div>
+    <Button type="button" label="Add task" icon="pi pi-plus" @click="addTask()" />
   </draggable>
 </template>
