7c386ca681a683374fc3c4cc044a7295e6e39853
javidev <jajavieralonso9@gmail.com>
2025-05-24 11:20:30 +0200

    Create a new dialog when you click on "ajustes"

--- src/components/misc/navbar/UserProfileComponent.vue
+++ src/components/misc/navbar/UserProfileComponent.vue
@@ -1,34 +1,24 @@
 <script>
 import Button from 'primevue/button';
 import Menu from 'primevue/menu';
+import Dialog from 'primevue/dialog';
 
 export default {
   name: 'UserProfileComponent',
   data() {
     return {
+      dialogVisible: false,
       items: [
         {
-          label: 'Documents',
+          label: 'Perfil',
           items: [
             {
-              label: 'New',
-              icon: 'pi pi-plus'
+              label: 'Ajustes',
+              icon: 'pi pi-cog',
+              command: () => { this.openSettings(); }
             },
             {
-              label: 'Search',
-              icon: 'pi pi-search'
-            }
-          ]
-        },
-        {
-          label: 'Profile',
-          items: [
-            {
-              label: 'Settings',
-              icon: 'pi pi-cog'
-            },
-            {
-              label: 'Logout',
+              label: 'Cerrar sesión',
               icon: 'pi pi-sign-out'
             }
           ]
@@ -36,13 +26,18 @@ export default {
       ]
     };
   },
-    components: {
+  components: {
     Button,
-    Menu
+    Menu,
+    Dialog
+
   },
   methods: {
     toggleMenu(event) {
       this.$refs.menu.toggle(event);
+    },
+    openSettings() {
+      this.dialogVisible = true;
     }
   }
 };
@@ -50,8 +45,20 @@ export default {
 </script>
 
 <template>
-
-  <Button icon="pi pi-user" severity="info" rounded aria-label="User" @click="toggleMenu"/>
+  <Button icon="pi pi-user" aria-label="User" @click="toggleMenu" class="bg-fuschia-100 !rounded-full"
+    style="border: none;" />
   <Menu ref="menu" :model="items" :popup="true" />
-
+  <Dialog v-model:visible="dialogVisible" position="full" modal :dismissableMask="true" header="Ajustes de Usuario">
+    <ul class="space-y-2 p-4">
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Cuenta</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Privacidad</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Idioma</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Tema</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Accesibilidad</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Seguridad</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Preferencias</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Soporte</li>
+      <li class="text-fuchsia-900 font-semibold hover:text-fuchsia-700 cursor-pointer">Acerca de</li>
+    </ul>
+  </Dialog>
 </template>
