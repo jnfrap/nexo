30194542cee1f309d978eb669ebc2213218ee7f6
jnfrap <jnfr97@gmail.com>
2025-06-02 20:32:55 +0200

    Remade app.vue reactivity to listen to user login, get only the boards of that user

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 import NavBarComponent from './components/misc/navbar/NavBarComponent.vue';
 import { ConfirmDialog, Toast } from 'primevue';
 import { storage } from './shared/storage';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, query, where, onSnapshot } from 'firebase/firestore';
 import { db } from './firebase/config';
 import { reorderBoarsdArray } from './shared/utils';
 
@@ -13,26 +14,49 @@ export default {
     ConfirmDialog,
     Toast
   },
+  data() {
+    return {
+      unsubscribeSnapshot: null,
+    };
+  },
   mounted() {
-    console.log(JSON.parse(JSON.stringify(storage)));
-    const boardsCollection = collection(db, "boards");
-    onSnapshot(boardsCollection, (querySnapshot) => {
-      storage.boards = [];
-      storage.filteredBoards = [];
-      querySnapshot.forEach((board) => {
-        storage.boards.push({
-          id: board.id,
-          ...board.data()
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (this.unsubscribeSnapshot) {
+        this.unsubscribeSnapshot();
+        this.unsubscribeSnapshot = null;
+      }
+
+      if (user) {
+        const boardsCollectionRef = collection(db, "boards");
+        const userBoardsQuery = query(boardsCollectionRef, where("userId", "==", user.uid));
+
+        this.unsubscribeSnapshot = onSnapshot(userBoardsQuery, (querySnapshot) => {
+          const boardsList = [];
+          querySnapshot.forEach((boardDoc) => {
+            boardsList.push({
+              id: boardDoc.id,
+              ...boardDoc.data()
+            });
+          });
+
+          storage.boards = reorderBoarsdArray(boardsList);
+          storage.filteredBoards = reorderBoarsdArray(boardsList);
+        }, (error) => {
+          console.error('Error ocurred while getting boards', error);
+          this.$toast.add({ severity: 'error', summary: 'Error', detail: 'Ocurrió un error al obtener los tableros.', life: 3000 });
         });
-        storage.filteredBoards.push({
-          id: board.id,
-          ...board.data()
-        });
-      });
-      storage.boards = reorderBoarsdArray(storage.boards);
-      storage.filteredBoards = reorderBoarsdArray(storage.filteredBoards);
+      } else {
+        storage.boards = [];
+        storage.filteredBoards = [];
+      }
     });
   },
+  beforeUnmount() {
+    if (this.unsubscribeSnapshot) {
+      this.unsubscribeSnapshot();
+    }
+  }
 }
 </script>
 
