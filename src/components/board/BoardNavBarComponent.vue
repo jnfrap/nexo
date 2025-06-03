e1da19e185c64d432b4427bbdbdd10d39bec7557
jnfrap <jnfr97@gmail.com>
2025-06-03 20:12:18 +0200

    Added star icon to indicate if is favorite

--- src/components/board/BoardNavBarComponent.vue
+++ src/components/board/BoardNavBarComponent.vue
@@ -28,9 +28,13 @@ export default {
 <template>
   <div class="flex items-center px-6 h-[50px] bg-[#b0b0b094] shadow-md">
     <div class="flex items-center gap-4">
-      <Button class="text-white cursor-pointer" icon="pi pi-arrow-left" @click="goBack"
-        aria-label="Back to boards" unstyled />
-      <h1 class="text-white text-xl">{{ board.title }}</h1>
+      <Button class="text-white cursor-pointer mt-1.5" icon="pi pi-arrow-left" @click="goBack" aria-label="Back to boards"
+        unstyled />
+      <h1 class="text-white text-2xl p-0">{{ board.title }}</h1>
+      <div class="text-yellow-400 mt-1.5">
+        <i v-if="board.isFavorite" class="pi pi-star-fill" title="Favorite" style="font-size: 1.5rem"></i>
+        <i v-else class="pi pi-star" title="No favorite" style="font-size: 1.5rem"></i>
+      </div>
     </div>
   </div>
 </template>
