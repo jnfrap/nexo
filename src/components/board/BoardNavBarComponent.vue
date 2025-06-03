c3ac145ab857a78119d2302a376eb4d424339131
jnfrap <jnfr97@gmail.com>
2025-06-03 20:06:13 +0200

    Added go back button

--- src/components/board/BoardNavBarComponent.vue
+++ src/components/board/BoardNavBarComponent.vue
@@ -1,5 +1,7 @@
+<!-- eslint-disable vue/no-reserved-component-names -->
 <script>
 import { getBoardByID } from '@/shared/services/boardService';
+import { Button } from 'primevue';
 
 export default {
   name: 'BoardNavBarComponent',
@@ -7,6 +9,14 @@ export default {
     return {
       board: {},
     };
+  },
+  components: {
+    Button
+  },
+  methods: {
+    async goBack() {
+      this.$router.push('/');
+    },
   },
   async mounted() {
     const boardId = this.$route.params.boardId;
@@ -16,8 +26,12 @@ export default {
 </script>
 
 <template>
-  <div class="flex items-center justify-between px-6 h-[50px] bg-[#b0b0b094] shadow-md">
-    <h1 class="text-white text-xl">{{ board.title }}</h1>
+  <div class="flex items-center px-6 h-[50px] bg-[#b0b0b094] shadow-md">
+    <div class="flex items-center gap-4">
+      <Button class="text-white cursor-pointer" icon="pi pi-arrow-left" @click="goBack"
+        aria-label="Back to boards" unstyled />
+      <h1 class="text-white text-xl">{{ board.title }}</h1>
+    </div>
   </div>
 </template>
 
